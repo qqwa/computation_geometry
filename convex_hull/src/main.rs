c1f3d130602371b984ca9fe7b09b655035c9d347
Benjamin Bäumler <benjamin.baeumler@qqwa.de>
2018-10-26 15:16:42 +0200

    add points with left mouse clicks
    splitted points and polygons into to vectors

--- convex_hull/src/main.rs
+++ convex_hull/src/main.rs
@@ -4,9 +4,11 @@ extern crate ggez;
 
 use ggez::*;
 use ggez::graphics::{DrawMode, Point2};
+use ggez::event::MouseButton;
 
 struct MainState {
     points: Vec<Point2>,
+    polygon: Vec<Point2>,
     point_color: graphics::Color,
     poly_color: graphics::Color,
 }
@@ -15,10 +17,7 @@ impl MainState {
     fn new(_ctx: &mut Context) -> GameResult<MainState> {
         let point_color = graphics::Color::from_rgb(255, 255, 255);
         let poly_color = graphics::Color::from_rgb(200, 50, 50);
-        let mut s = MainState {points: Vec::new(), point_color, poly_color };
-        s.points.push(Point2::new(100.0, 100.0));
-        s.points.push(Point2::new(200.0, 150.0));
-        s.points.push(Point2::new(100.0, 200.0));
+        let mut s = MainState {points: Vec::new(), polygon: Vec::new(), point_color, poly_color };
         Ok(s)
     }
 }
@@ -34,11 +33,29 @@ impl event::EventHandler for MainState {
         for point in &self.points {
             graphics::circle(ctx, DrawMode::Fill, point.clone(), 2.5, 0.15)?;
         }
+
+        let mut polygon: Vec<Point2> = Vec::new();
+        for point in &self.points {
+            if point[0] < 100.0 {
+                polygon.push(point.clone())
+            }
+        }
+
         graphics::set_color(ctx, self.poly_color)?;
-        graphics::polygon(ctx, DrawMode::Line(2.0), &self.points[..])?;
+        graphics::polygon(ctx, DrawMode::Line(2.0), &self.polygon[..])?;
 
         graphics::present(ctx);
         Ok(())
+    }
+
+    fn mouse_button_down_event(&mut self, _ctx: &mut Context, button: MouseButton, x: i32, y: i32) {
+    }
+
+    fn mouse_button_up_event(&mut self, _ctx: &mut Context, button: MouseButton, x: i32, y: i32) {
+        if button != MouseButton::Left {
+            return;
+        }
+        self.points.push(Point2::new(x as f32, y as f32));
     }
 }
 
