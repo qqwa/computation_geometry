4b9f14631ad13adfefb9898a099dcce3a4dad1f9
Benjamin Bäumler <benjamin.baeumler@qqwa.de>
2018-10-27 23:43:43 +0200

    graham's scan, lower bound doesn't work 100%

--- convex_hull/src/main.rs
+++ convex_hull/src/main.rs
@@ -16,19 +16,71 @@ struct MainState {
     polygon: Vec<Point2>,
     point_color: graphics::Color,
     poly_color: graphics::Color,
+    dirty_flag: bool,
 }
 
 impl MainState {
     fn new(_ctx: &mut Context) -> GameResult<MainState> {
         let point_color = graphics::Color::from_rgb(255, 255, 255);
         let poly_color = graphics::Color::from_rgb(200, 50, 50);
-        let mut s = MainState {points: Vec::new(), polygon: Vec::new(), point_color, poly_color };
+        let mut s = MainState {points: Vec::new(), polygon: Vec::new(), point_color, poly_color, dirty_flag: false };
         Ok(s)
     }
 }
 
+fn left_turn(points: &[Point2]) -> bool {
+    if points.len() != 3 {
+        panic!("tried to calculate left turn for {} points instead of 3", points.len());
+    }
+    let a = Point2::new(points[1][0]-points[0][0], points[1][1]-points[0][1]);
+    let b = Point2::new(points[2][0]-points[0][0], points[2][1]-points[0][1]);
+
+    let left = (a[0]*b[1] - b[0]*a[1]) < 0.0;
+    left
+}
+
 impl event::EventHandler for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        if self.dirty_flag {
+            self.dirty_flag = false;
+            debug!("Recomputed convex hull:");
+            self.points.sort_by(|a, b| a[0].partial_cmp(&b[0]).unwrap());
+            debug!("Left point: {}", self.points[0]);
+            debug!("Right pont: {}", self.points[self.points.len()-1]);
+
+            if self.points.len() < 3 {
+                return Ok(())
+            }
+
+            let mut upper = Vec::new();
+            upper.extend_from_slice(&self.points[..2]);
+
+            for point in &self.points[2..] {
+                upper.push(point.clone());
+                while 2 < upper.len() && left_turn(&upper[upper.len()-3..]) {
+                    upper.remove(upper.len()-2);
+                }
+            }
+
+            let mut lower: Vec<Point2> = Vec::new();
+            lower.extend_from_slice(&self.points[self.points.len()-2..]);
+            lower.reverse();
+            for point in self.points[1..self.points.len()-2].iter().rev() {
+                lower.push(point.clone());
+                while 2 < lower.len() && left_turn(&lower[lower.len()-3..]) {
+                    lower.remove(lower.len()-2);
+                }
+            }
+            lower.remove(0);
+            lower.pop();
+            // debug!("{:#?}", lower);
+            // debug!("lower for first value: {:?}, len: {}", self.points[1..self.points.len()-1].iter().rev().nth(0), self.points[1..self.points.len()-2].len());
+
+            self.polygon.truncate(0);
+            self.polygon.append(&mut upper);
+            self.polygon.append(&mut lower);
+
+        }
         Ok(())
     }
 
@@ -64,10 +116,18 @@ impl event::EventHandler for MainState {
         if !self.points.contains(&point) {
             debug!("Created Point: {}", point);
             self.points.push(point);
+            self.dirty_flag = true;
         } else {
             debug!("Removed Point: {}", point);
             self.points.remove_item(&point);
+            self.dirty_flag = true;
         }
+
+        // self.polygon.truncate(0);
+        // for point in self.points.iter().step_by(2) {
+        //     self.polygon.push(point.clone());
+        // }
+
     }
 }
 
