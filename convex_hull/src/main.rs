6441143b4013247e714b170b189c5b8cc27d0725
Benjamin Bäumler <benjamin.baeumler@qqwa.de>
2018-10-28 00:32:48 +0200

    rustfmt

--- convex_hull/src/main.rs
+++ convex_hull/src/main.rs
@@ -1,15 +1,15 @@
 #![feature(box_syntax)]
 #![feature(vec_remove_item)]
 
-extern crate ggez;
-extern crate fern;
 extern crate chrono;
+extern crate fern;
+extern crate ggez;
 #[macro_use]
 extern crate log;
 
-use ggez::*;
-use ggez::graphics::{DrawMode, Point2};
 use ggez::event::MouseButton;
+use ggez::graphics::{DrawMode, Point2};
+use ggez::*;
 
 struct MainState {
     points: Vec<Point2>,
@@ -23,26 +23,34 @@ impl MainState {
     fn new(_ctx: &mut Context) -> GameResult<MainState> {
         let point_color = graphics::Color::from_rgb(255, 255, 255);
         let poly_color = graphics::Color::from_rgb(200, 50, 50);
-        let mut s = MainState {points: Vec::new(), polygon: Vec::new(), point_color, poly_color, dirty_flag: false };
+        let mut s = MainState {
+            points: Vec::new(),
+            polygon: Vec::new(),
+            point_color,
+            poly_color,
+            dirty_flag: false,
+        };
         Ok(s)
     }
 }
 
 fn left_turn(points: &[Point2]) -> bool {
     if points.len() != 3 {
-        panic!("tried to calculate left turn for {} points instead of 3", points.len());
+        panic!(
+            "tried to calculate left turn for {} points instead of 3",
+            points.len()
+        );
     }
-    let a = Point2::new(points[1][0]-points[0][0], points[1][1]-points[0][1]);
-    let b = Point2::new(points[2][0]-points[0][0], points[2][1]-points[0][1]);
+    let a = Point2::new(points[1][0] - points[0][0], points[1][1] - points[0][1]);
+    let b = Point2::new(points[2][0] - points[0][0], points[2][1] - points[0][1]);
 
-    let left = (a[0]*b[1] - b[0]*a[1]) < 0.0;
+    let left = (a[0] * b[1] - b[0] * a[1]) < 0.0;
     left
 }
 
 impl event::EventHandler for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
-            
-        // // test points with multiple points that have the same x value 
+        // // test points with multiple points that have the same x value
         // if self.points.len() == 0 {
         //     self.points.extend(everything_is_convex());
         //     self.dirty_flag = true;
@@ -51,13 +59,16 @@ impl event::EventHandler for MainState {
         if self.dirty_flag {
             self.dirty_flag = false;
             debug!("Recomputed convex hull:");
-            self.points.sort_by(|a, b| a[0].partial_cmp(&b[0]).unwrap().then_with(|| a[1].partial_cmp(&b[1]).unwrap()));
+            self.points.sort_by(|a, b| {
+                a[0].partial_cmp(&b[0])
+                    .unwrap()
+                    .then_with(|| a[1].partial_cmp(&b[1]).unwrap())
+            });
             debug!("Left point: {}", self.points[0]);
-            debug!("Right pont: {}", self.points[self.points.len()-1]);
-
+            debug!("Right pont: {}", self.points[self.points.len() - 1]);
 
             if self.points.len() < 3 {
-                return Ok(())
+                return Ok(());
             }
 
             let mut upper = Vec::new();
@@ -65,18 +76,18 @@ impl event::EventHandler for MainState {
 
             for point in &self.points[2..] {
                 upper.push(point.clone());
-                while 2 < upper.len() && left_turn(&upper[upper.len()-3..]) {
-                    upper.remove(upper.len()-2);
+                while 2 < upper.len() && left_turn(&upper[upper.len() - 3..]) {
+                    upper.remove(upper.len() - 2);
                 }
             }
 
             let mut lower: Vec<Point2> = Vec::new();
-            lower.extend_from_slice(&self.points[self.points.len()-2..]);
+            lower.extend_from_slice(&self.points[self.points.len() - 2..]);
             lower.reverse();
-            for point in self.points[..self.points.len()-2].iter().rev() {
+            for point in self.points[..self.points.len() - 2].iter().rev() {
                 lower.push(point.clone());
-                while 2 < lower.len() && left_turn(&lower[lower.len()-3..]) {
-                    lower.remove(lower.len()-2);
+                while 2 < lower.len() && left_turn(&lower[lower.len() - 3..]) {
+                    lower.remove(lower.len() - 2);
                 }
             }
             lower.remove(0);
@@ -85,7 +96,6 @@ impl event::EventHandler for MainState {
             self.polygon.truncate(0);
             self.polygon.append(&mut upper);
             self.polygon.append(&mut lower);
-
         }
         Ok(())
     }
@@ -133,19 +143,19 @@ impl event::EventHandler for MainState {
         // for point in self.points.iter().step_by(2) {
         //     self.polygon.push(point.clone());
         // }
-
     }
 }
 
 fn everything_is_convex() -> Vec<Point2> {
-    vec![Point2::new(200.0, 100.0), 
-         Point2::new(250.0, 100.0), 
-         Point2::new(300.0, 150.0),
-         Point2::new(250.0, 160.0),
-         Point2::new(250.0, 170.0),
-         Point2::new(250.0, 180.0),
-         Point2::new(250.0, 190.0),
-         Point2::new(250.0, 120.0),
+    vec![
+        Point2::new(200.0, 100.0),
+        Point2::new(250.0, 100.0),
+        Point2::new(300.0, 150.0),
+        Point2::new(250.0, 160.0),
+        Point2::new(250.0, 170.0),
+        Point2::new(250.0, 180.0),
+        Point2::new(250.0, 190.0),
+        Point2::new(250.0, 120.0),
     ]
 }
 
