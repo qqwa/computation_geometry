a2b10101400eb2307c07e668676b5fa6975059ab
Benjamin Bäumler <benjamin.baeumler@qqwa.de>
2018-11-26 19:19:40 +0100

    clean up

--- src/states/line_state.rs
+++ src/states/line_state.rs
@@ -1,4 +1,3 @@
-use ggez::event::MouseButton;
 use ggez::graphics::{DrawMode, Point2};
 use ggez::*;
 
@@ -8,6 +7,7 @@ use crate::intersection;
 pub struct LineState {
     lines: Vec<(Point2, Point2)>,
     intersection: Vec<(Point2, Point2)>,
+    intersection_points: Vec<Point2>,
     point_of_line: Option<Point2>,
     color: graphics::Color,
     line_color: graphics::Color,
@@ -24,6 +24,7 @@ impl LineState {
         LineState {
             lines: Vec::new(),
             intersection: Vec::new(),
+            intersection_points: Vec::new(),
             point_of_line: None,
             color,
             line_color,
@@ -38,7 +39,9 @@ impl Scene<SharedState, Event> for LineState {
     fn update(&mut self, _state: &mut SharedState) -> SceneSwitch<SharedState, Event> {
         if self.dirty_flag {
             self.dirty_flag = false;
-            self.intersection = intersection::iso_scan_line(&self.lines[..]);
+            let (a, b) = intersection::iso_scan_line(&self.lines[..]);
+            self.intersection = a;
+            self.intersection_points = b;
         }
 
         if self.close {
@@ -50,11 +53,13 @@ impl Scene<SharedState, Event> for LineState {
     fn draw(&mut self, state: &mut SharedState, ctx: &mut ggez::Context) -> ggez::GameResult<()> {
         graphics::clear(ctx);
 
+        // draw point of unfinished line
         graphics::set_color(ctx, self.color)?;
         if let Some(point) = self.point_of_line {
             graphics::circle(ctx, DrawMode::Fill, point, 2.5, 0.15)?;
         }
 
+        // draw lines
         graphics::set_color(ctx, self.line_color)?;
         for (p1, p2) in &self.lines {
             graphics::circle(ctx, DrawMode::Fill, p1.clone(), 2.5, 0.15)?;
@@ -62,11 +67,18 @@ impl Scene<SharedState, Event> for LineState {
             graphics::line(ctx, &vec![*p1, *p2][..], 1.0)?;
         }
 
+        // redraw lines in different color that intersect
         graphics::set_color(ctx, self.intersection_color)?;
         for (p1, p2) in &self.intersection {
             graphics::circle(ctx, DrawMode::Fill, p1.clone(), 2.5, 0.15)?;
             graphics::circle(ctx, DrawMode::Fill, p2.clone(), 2.5, 0.15)?;
             graphics::line(ctx, &vec![*p1, *p2][..], 1.0)?;
+        }
+
+        // draw intersections points
+        graphics::set_color(ctx, self.color)?;
+        for p in &self.intersection_points {
+            graphics::circle(ctx, DrawMode::Fill, p.clone(), 2.5, 0.15)?;
         }
 
         graphics::present(ctx);
@@ -118,81 +130,5 @@ impl Scene<SharedState, Event> for LineState {
     }
     fn draw_previous(&self) -> bool {
         false
-    }
-}
-
-impl event::EventHandler for LineState {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
-        if self.dirty_flag {
-            self.dirty_flag = false;
-            // TODO: Do line scan
-            self.intersection = intersection::iso_scan_line(&self.lines[..]);
-        }
-        Ok(())
-    }
-
-    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        graphics::clear(ctx);
-
-        graphics::set_color(ctx, self.color)?;
-        if let Some(point) = self.point_of_line {
-            graphics::circle(ctx, DrawMode::Fill, point, 2.5, 0.15)?;
-        }
-
-        graphics::set_color(ctx, self.line_color)?;
-        for (p1, p2) in &self.lines {
-            graphics::circle(ctx, DrawMode::Fill, p1.clone(), 2.5, 0.15)?;
-            graphics::circle(ctx, DrawMode::Fill, p2.clone(), 2.5, 0.15)?;
-            graphics::line(ctx, &vec![*p1, *p2][..], 1.0)?;
-        }
-
-        graphics::set_color(ctx, self.intersection_color)?;
-        for (p1, p2) in &self.intersection {
-            graphics::circle(ctx, DrawMode::Fill, p1.clone(), 2.5, 0.15)?;
-            graphics::circle(ctx, DrawMode::Fill, p2.clone(), 2.5, 0.15)?;
-            graphics::line(ctx, &vec![*p1, *p2][..], 1.0)?;
-        }
-
-        graphics::present(ctx);
-        Ok(())
-    }
-
-    fn mouse_button_up_event(&mut self, _ctx: &mut Context, button: MouseButton, x: i32, y: i32) {
-        if button != MouseButton::Left {
-            if button == MouseButton::Right {
-                self.point_of_line = None;
-            }
-            return;
-        }
-
-        let point = Point2::new(x as f32, y as f32);
-
-        if self.point_of_line.is_none() {
-            self.point_of_line = Some(point);
-        } else {
-            let cur_point = self.point_of_line.unwrap();
-            let mut x_distance = cur_point.x - point.x;
-            let mut y_distance = cur_point.y - point.y;
-            if x_distance < 0.0 {
-                x_distance *= -1.0;
-            }
-            if y_distance < 0.0 {
-                y_distance *= -1.0;
-            }
-            let point = if x_distance < y_distance {
-                // take y value of new point
-                Point2::new(cur_point.x, point.y)
-            } else {
-                // take x value of new point
-                Point2::new(point.x, cur_point.y)
-            };
-            if cur_point.x < point.x {
-                self.lines.push((cur_point, point));
-            } else {
-                self.lines.push((point, cur_point));
-            }
-            self.point_of_line = None;
-            self.dirty_flag = true;
-        }
     }
 }
--- src/states/point_state.rs
+++ src/states/point_state.rs
@@ -1,10 +1,7 @@
-use ggez::event::MouseButton;
 use ggez::graphics::{DrawMode, Point2};
 use ggez::*;
 
 use super::*;
-
-use crate::convex_hull::{grahams_scan, jarvis_march};
 
 #[derive(Clone)]
 pub struct PointState {
@@ -83,55 +80,5 @@ impl Scene<SharedState, Event> for PointState {
     }
     fn draw_previous(&self) -> bool {
         false
-    }
-}
-
-impl event::EventHandler for PointState {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
-        // // test points with multiple points that have the same x value
-        // if self.points.len() == 0 {
-        //     self.points.extend(everything_is_convex());
-        //     self.dirty_flag = true;
-        // }
-
-        if self.dirty_flag {
-            self.dirty_flag = false;
-            // self.polygon = grahams_scan(&self.points);
-            // self.polygon = jarvis_march(&self.points);
-            self.polygon = (self.get_convex_hull)(&self.points);
-        }
-        Ok(())
-    }
-
-    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        graphics::clear(ctx);
-        graphics::set_color(ctx, self.point_color)?;
-        for point in &self.points {
-            graphics::circle(ctx, DrawMode::Fill, point.clone(), 2.5, 0.15)?;
-        }
-
-        graphics::set_color(ctx, self.poly_color)?;
-        graphics::polygon(ctx, DrawMode::Line(2.0), &self.polygon[..])?;
-
-        graphics::present(ctx);
-        Ok(())
-    }
-
-    //fn mouse_button_down_event(&mut self, _ctx: &mut Context, button: MouseButton, x: i32, y: i32) {}
-
-    fn mouse_button_up_event(&mut self, _ctx: &mut Context, button: MouseButton, x: i32, y: i32) {
-        if button != MouseButton::Left {
-            return;
-        }
-        let point = Point2::new(x as f32, y as f32);
-        if !self.points.contains(&point) {
-            debug!("Created Point: {}", point);
-            self.points.push(point);
-            self.dirty_flag = true;
-        } else {
-            debug!("Removed Point: {}", point);
-            self.points.remove_item(&point);
-            self.dirty_flag = true;
-        }
     }
 }
